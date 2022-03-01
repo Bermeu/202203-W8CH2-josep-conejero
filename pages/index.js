d6db0c7c077d267effad8130741f05d2e8f2bd1e
Josep Conejero <bermeu@gmail.com>
2022-03-01 21:02:58 +0100

    Added favicon pero no se ve

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,43 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+//import styleComponent from "style";
 
 export default function Home() {
   return (
     <>
-      <p>HOME</p>
+      <p>POKEMON</p>
+      <p>
+        Pokémon (ポケモン) es una franquicia de medios que originalmente comenzó
+        como un videojuego RPG, pero debido a su popularidad ha logrado
+        expandirse a otros medios de entretenimiento como series de televisión,
+        películas, juegos de cartas, ropa, entre otros, convirtiéndose en una
+        marca que es reconocida en el mercado mundial. Las ventas de videojuegos
+        hasta el 1 de diciembre de 2006 habían alcanzado una cantidad de 340
+        millones de ejemplares (incluyendo la venta de la versión Pikachu de la
+        consola Nintendo 64), logrando ocupar el segundo lugar de las sagas de
+        videojuegos más vendidos de Nintendo.
+      </p>
+      <p>
+        La franquicia celebró su décimo aniversario el 27 de febrero de 2006. La
+        saga de videojuegos es desarrollada por la compañía programadora de
+        software japonesa Game Freak, con personajes creados por Satoshi Tajiri
+        para la empresa de juguetes Creatures Inc., y a su vez distribuida por
+        Nintendo. La misión en estos juegos es capturar y entrenar a los pokémon
+        (criaturas cuya denominación da nombre al juego), que hasta la fecha
+        alcanzan el número de 890. La posibilidad de intercambiarlos le hizo
+        conseguir una popularidad que se plasmó en un éxito de ventas y la
+        consiguiente aparición de una serie animada, películas y diverso
+        merchandising como peluches, juguetes y cartas.
+      </p>
+      <p>
+        La producción de los videojuegos, serie de anime y demás material para
+        su distribución en occidente fue realizada en Estados Unidos por 4Kids
+        Entertainment hasta noviembre de 2005, momento en que decidió no renovar
+        su contrato con Pokémon USA (una subsidiaria de Pokémon Company).
+        Actualmente esta supervisa todo lo referente al material de Pokémon en
+        su distribución en occidente.
+      </p>
     </>
   );
 }
